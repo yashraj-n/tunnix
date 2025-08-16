a17b0a0cce5a6d1cd764ede206ae85c4c5358370
Yashraj Narke <66460598+yashraj-n@users.noreply.github.com>
2025-08-16 19:56:57 +0530

    bug: remove alpine msg by overwriting motd

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,11 +6,13 @@ if [ -n "$SSH_API_KEY" ]; then
 fi
 
 # MOTD
+echo "" > /etc/motd
 echo "--------------------------------" >> /etc/motd
 echo "Welcome to Tunnix" >> /etc/motd
 echo "Please start this repo if you find this project useful https://github.com/yashraj-n/tunnix" >> /etc/motd
 echo "--------------------------------" >> /etc/motd
 echo "Use this URL: $TUNNIX_FQDN" >> /etc/motd
+echo "" >> /etc/motd
 
 echo "Starting SSH daemon on port 12000"
 exec /usr/sbin/sshd -D -e
